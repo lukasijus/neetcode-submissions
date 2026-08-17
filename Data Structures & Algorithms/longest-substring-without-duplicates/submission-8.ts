680e50fda578dfd08cde419bc43b9995d757aebf
Lukas Rimkus <lukasrimkuslg@gmail.com>
2026-08-18 00:11:12 +0300

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let left = 0
+        let right = 1
+        if(s.length <= 1 ) return s.length
+        let set = new Set(s[0])
+        let max = 0 
+        while(right < s.length) {
+            if(set.has(s[right])) {
+                set.delete(s[left])
+                left++
+            }
+            else if(!set.has(s[right])){
+                set.add(s[right])
+                right++
+            }
+            if(set.size > max) {
+                max = set.size 
+            }
+        }
+        return Math.max(max,set.size)
+    }
+}
