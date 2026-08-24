0c715da3c82a8031b3d5c879d5344e4e1b91f168
Lukas Rimkus <lukasrimkuslg@gmail.com>
2026-08-25 01:46:08 +0300

    Add: valid-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root: TreeNode | null): boolean {
+        if(!root) return true 
+        function validate(node: TreeNode | null, min , max ) {
+            if(!node) return true 
+            if(node.val <= min || node.val >= max) {
+                return false 
+            }
+            return(validate(node.left, min, node.val) && validate(node.right, node.val, max))
+        }      
+        return validate(root, -Infinity, Infinity)
+    }
+}
