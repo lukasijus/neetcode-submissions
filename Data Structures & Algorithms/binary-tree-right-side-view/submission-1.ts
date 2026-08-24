3999b631311416b9b4bb1d5a3fed07671d41df27
Lukas Rimkus <lukasrimkuslg@gmail.com>
2026-08-24 23:46:03 +0300

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root: TreeNode | null): number[] {
+        let arr: number[] = []
+
+        function rsv(node: TreeNode | null, level) : boolean {
+            if(!node) return false 
+            
+            if(node.left && node.right) {
+                if(!arr[level]) {
+                    arr[level] = node.val
+                }
+
+                rsv(node.right, level + 1)
+                rsv(node.left, level + 1)
+            }
+            else if(node.right) {
+                if(!arr[level]) {
+                    arr[level] = node.val
+                }
+                rsv(node.right, level + 1)
+            } else {
+                if(!arr[level]) {
+                    arr[level] = node.val
+                }
+                rsv(node.left, level + 1)
+            }
+        }
+        rsv(root,0)
+
+        return arr
+    }
+
+}
