86fbb6555c0a1d31aec3eeb926505e7bd707de99
Lukas Rimkus <lukasrimkuslg@gmail.com>
2026-09-18 15:07:36 +0300

    Add: range-sum-query-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-1.ts
@@ -0,0 +1,19 @@
+class NumArray {
+    /**
+     * @param {number[]} nums
+     */
+    nums:number[]
+    constructor(nums: number[]) {
+        this.nums = nums
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left: number, right: number): number {
+        const arr = this.nums.slice(left,right+1)
+        return arr.reduce((a,b) => a + b )
+    }
+}
